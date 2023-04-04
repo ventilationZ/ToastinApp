57065e29c7e402bad79bdc7baef0a152937dfb46
eMobilis <elikibetael4@gmail.com>
2023-04-04 09:56:41 +0300

    Toastng in Android

---
+++ app/src/main/java/com/example/toastingapp_mutai/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.toastingapp_mutai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var display_text:TextView
+    private lateinit var edt_input_data:EditText
+    private lateinit var submitbutton:Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        display_text= findViewById(R.id.txtdisplay)
+        edt_input_data= findViewById(R.id.edtdata)
+        submitbutton= findViewById(R.id.btnsubmit)
+
+        submitbutton.setOnClickListener {
+         Toast.makeText(this,"Elijah",Toast.LENGTH_S)}
+
+    }
+}
